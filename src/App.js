56ed0e8d202f295ebb36514d71aa5be5928fb35e
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-01-23 06:54:49 +0000

    Paste the API URL from codegecko.com

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=idr&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
 function App() {
   return (
     <div className="app">
